c97bc20d6c57d9263f4da1c71bdfdd203a811be2
moustapha93210 <moustapha.chaib.sio@gmail.com>
2024-09-03 02:26:34 +0200

    Code - Langage C-4 - Structures répétitives  Les boucles  for, while, et do ... while

---
+++ Langage C-4 - Structures répétitives  Les boucles  for, while, et do ... while/boucle_while.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, i;
+    i = 0;
+
+    printf("Veuillez entrer un nombre : ");
+    scanf("%d", &N);
+
+    while(N < 0 || N > 10) {
+
+        printf("Veuillez entrer un nombre : ");
+        scanf("%d", &N);
+
+    }
+
+    //1ère manière d'afficher la table de multiplication
+    while(i <= 10) {
+
+        printf("%d * %d = %d \n", N, i, N * i);
+        i++;
+    }
+
+    //2ème manière d'afficher la table de multiplication
+    for(i = 0; i <= 10; i++) {
+
+        printf("%d * %d = %d\n", N, i, N*i);
+
+    }
+
+    return 0;
+}
