0f59828df88e464e234a98dccec3185776545d2e
moustapha93210 <moustapha.chaib.sio@gmail.com>
2024-09-05 01:37:56 +0200

    05/09/2024-01h37

--- Exercices corrigés en C/Langage C-9 - Exercices/Exercice 93/main.c
+++ Exercices corrigés en C/Langage C-9 - Exercices/Exercice 93/main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 
 
 int main(){
 
+    char mot[20];
+
+    // Saisie du mot par l'utilisateur
+    puts("Veuillez entrer un mot :");
+    gets(mot);
+
+    int i, n;
+
+    n = 0;
+
+    for(i = 0; mot[i] != '\0' ; i++)
+        n++;
+
+
+    printf("La taille de la chaine est : %d\n", n);
+    printf("La taille de la chaine est : %d", strlen(mot) );
 
 
 
--- Exercices corrigés en C/Langage C-9 - Exercices/Exercice 93/programme_ChaineTrouverLongueurSansStrlen.c
+++ Exercices corrigés en C/Langage C-9 - Exercices/Exercice 93/programme_ChaineTrouverLongueurSansStrlen.c
@@ -1,12 +1,67 @@
 #include <stdio.h>
+#include <string.h>
 
 
 
 int main(){
 
+    char C[100];
+    int i, n;
+
+
+    // Saisie du mot par l'utilisateur
+    puts("Veuillez entrer un mot :");
+    gets(C);
+
+    n = 0;
+
+    for(i = 0; C[i] != '\0' ; i++){
+
+            n++;
+
+        printf("i = %d\n", i);
+        printf("C[%d] = %c\n\n", i, C[i]);
+
+    }
+
+    printf("Taille : %d\n", n);
+    printf("Taille avec la fonction 'strlen()' : %d", strlen(C) );
 
 
 
 
     return 0;
 }
+
+
+/*
+ * Exercice fais avec des pointeurs
+
+
+    char c[100];
+    int i, n;
+    char *p;
+
+
+    // Saisie du mot par l'utilisateur
+    puts("Veuillez entrer un mot :");
+    gets(c);
+
+    p = c;
+
+    n = 0;
+
+    for(p = c; p < c + 100; p++){
+
+    if(*p != '\0'){
+    n++;
+
+    }
+
+    printf("i = %d\n", p - c);
+    printf("C[%d] = %c\n\n", p - c, *p);
+
+    }
+
+    printf("Taille : %d", n);
+*/
